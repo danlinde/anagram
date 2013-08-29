0161843fc9c54ba2d60e73b5d3a2f3a1dbb114b8
Margo <margo@margonline.co.uk>
2013-08-29 17:34:34 +0100

    removed test checking input as array

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,10 +2,6 @@ require_relative '../anagram'
 describe Anagram do
 let(:anagrams) {Anagram.new(%w(mary army listen silent banana))}
 
-  it 'should take an array as input' do
-    words = %w(rats mary tars star banana silent listen tensil)
-    expect(Anagram.new(words)).to_not raise_error
-  end
 
   it 'should be able to sort a word\'s letters alphabetically' do
     anagrams.sort_word('tars').should eq 'arst'  
